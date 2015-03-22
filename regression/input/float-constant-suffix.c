d58dc92836234614bb71b18b36a563f8d3f1560b
Petr P?sa? <ppisar@redhat.com>
2015-03-22 16:56:54 +0100

    Do not split decimal float suffix from constant N1312 draft of ISO/IEC WDTR24732 defines additional floating types with given suffixes:
    _Decimal32  DF, df
    _Decimal64  DD, dd
    _Decimal128 DL, dl
    
    These suffixes must stick on numeric part of the constant as classic
    float or long float does.

---
+++ regression/input/float-constant-suffix.c
@@ -0,0 +1,13 @@
+float foo = 1.0F;
+float foo = 1.0f;
+double foo = 1.0;
+double foo = 1.0;
+long double foo = 1.0L;
+long double foo = 1.0l;
+
+_Decimal32 foo = 1.0DF;
+_Decimal32 foo = 1.0df;
+_Decimal64 foo = 1.0DD;
+_Decimal64 foo = 1.0dd;
+_Decimal128 foo = 1.0DL;
+_Decimal128 foo = 1.0dl;
